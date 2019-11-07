b5a62eb94110c8f49a84675fae2f5f748f04ea66
Sourav Biswas <sourav.biswas59@gmail.com>
2019-11-07 22:09:27 +0530

    this is a Push to Module

---
+++ hello_world/src/Controller/HelloWorldController.php
@@ -0,0 +1,10 @@
+<?php
+namespace Drupal\hello_world\Controller;
+class HelloWorldController {
+    public function hello() {
+        return array(
+                '#title' => 'Hello World!',
+                '#markup' => 'Here is some content.',
+            );
+    }
+}
